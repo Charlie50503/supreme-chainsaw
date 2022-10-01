6c26ab2cf50ec04a4b4bd3534476ea8bd1ea36ac
charlie50503 <cate50503@gmail.com>
2022-10-01 16:32:58 +0900

    228-1: 建立基本 formGroup 代碼

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,3 +1,5 @@
+import { RecipeService } from './../recipe.service';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
@@ -10,7 +12,9 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
 
-  constructor(private route: ActivatedRoute) { }
+  recipeForm:FormGroup;
+  constructor(private route: ActivatedRoute,
+    private recipeService:RecipeService) { }
 
   ngOnInit() {
     this.route.params
@@ -18,8 +22,28 @@ export class RecipeEditComponent implements OnInit {
         (params: Params) => {
           this.id = +params['id'];
           this.editMode = params['id'] != null;
+          this.initForm();
         }
       );
+  }
+
+  initForm(){
+    let recipeName = "";
+    let recipeImgPath = "";
+    let recipeDescription = "";
+
+    if(this.editMode){
+      const recipe = this.recipeService.getRecipe(this.id)
+      recipeName = recipe.name
+      recipeImgPath = recipe.imagePath
+      recipeDescription = recipe.description
+    }
+
+    this.recipeForm = new FormGroup({
+      "name": new FormControl(recipeName),
+      "imagePath": new FormControl(recipeImgPath),
+      "description": new FormControl(recipeDescription),
+    })
   }
 
 }
